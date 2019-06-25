ae84e14ca8f2fc0df89b750698e8aa45c18f7ecf
joao.serrano <jcmserrano@sapo.pt>
2019-06-25 10:23:03 +0100

    Juntando o conversao

---
+++ src/pt/com/everis/academia/java/Conversao.java
@@ -0,0 +1,31 @@
+package pt.com.everis.academia.java;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+
+		/* Exemplo não possivel de conversão directa
+		 * 
+		 * double d = 3.1415;
+		 * int i = d;
+		 */
+		
+		/*
+		int i2 = 5;
+		
+		double d2 = i;
+		
+		System.out.println(d2);
+		*/
+		
+		/* Exemplo de conversão que não é errada
+		long l3 = 12345678901l;
+		int i3 = (int) l3;
+		System.out.println(i3);
+		*/
+		
+		Boolean boolean1= new Boolean (true);
+		System.out.println(boolean1);
+	}
+
+}
